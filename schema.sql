e736dddf5de42de91a169ca9a46f2233c97c53ca
Alex81052 <alxdd87@gmail.com>
2021-12-05 19:42:59 -0500

    update

--- schema.sql
+++ schema.sql
@@ -1,10 +1,11 @@
-*Creating tables for PH-EmployeeDB
+-- Creating tables for PH-EmployeeDB
 CREATE TABLE departments (
      dept_no VARCHAR(4) NOT NULL,
      dept_name VARCHAR(40) NOT NULL,
      PRIMARY KEY (dept_no),
      UNIQUE (dept_name)
 );
+select * from departments
 
 CREATE TABLE employees (
      emp_no INT NOT NULL,
@@ -37,15 +38,19 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
+select * from salaries
+
+
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
-	title INT NOT NULL,
+	title VARCHAR(40) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, title, from_date)
 	
 );
+select * from titles
 
 CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
@@ -56,3 +61,4 @@ CREATE TABLE dept_emp (
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
+select * from dept_emp
